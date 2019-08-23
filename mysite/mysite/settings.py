6308c8b9345661c0477347b4912c0eb6f301d9a7
Peter Casey <casey.peter10@gmail.com>
2019-08-23 10:15:56 -0700

    modified:   mysite/mysite/settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'msyite.polls.apps.PollsConfig',
+    'mysite.polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
